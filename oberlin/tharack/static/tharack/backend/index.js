261d7e9c4848a5a5524b25bfea7cc458db8df3fe
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2023-11-26 23:35:31 -0500

    Add files via upload

---
+++ oberlin/tharack/static/tharack/backend/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('h2', {
+    strings: ["Software engineer"],
+    typeSpeed: 60,
+    backSpeed : 60,
+    loop:true
+    });
---
+++ oberlin/tharack/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+
+def home(request):
+    return render(request,"tharack/home.html")
+def about(request):
+    return render(request,"tharack/about.html")
+def contact(request):
+    return render(request,"tharack/contact.html")
