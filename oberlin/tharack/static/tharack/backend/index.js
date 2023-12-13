fca1d347c646f02b1c5d776d4be592074376cf20
tharcissentirandekura <tntirand@oberlin.edu>
2023-12-13 16:35:17 -0500

    save

--- oberlin/tharack/static/tharack/backend/index.js
+++ oberlin/tharack/static/tharack/backend/index.js
@@ -1,6 +1,17 @@
-var typed = new Typed('h2', {
-    strings: ["Software engineer"],
-    typeSpeed: 60,
-    backSpeed : 60,
-    loop:true
-    });
+// var typed = new Typed('h2', {
+//     strings: ["Software engineer"],
+//     typeSpeed: 60,
+//     backSpeed : 60,
+//     loop:true
+//     });
+
+function main(){
+    const integers = 9;
+    for(let i = 0;i < 9;i ++){
+        setInterval(10){
+            console.log(i);
+        }
+        
+    }
+}
+main()
