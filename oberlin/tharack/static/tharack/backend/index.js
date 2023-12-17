b2eac373b81edf33801dd6678ad7e2b1abf53829
tharcissentirandekura <tntirand@oberlin.edu>
2023-12-16 20:32:34 -0500

    save the changes for javascript

--- oberlin/tharack/static/tharack/backend/index.js
+++ oberlin/tharack/static/tharack/backend/index.js
@@ -1,15 +1,11 @@
 function change() {
     document.querySelector("#backs").onclick = () => {
-      console.log("names");
-      console.log("clicked");
-  
+
       if (document.querySelector("body").style.backgroundColor === "white") {
         // change color and background color of the body
-        console.log("color white");
         document.querySelector("body").style.backgroundColor = "black";
         document.querySelector("body").style.color = "white";
       } else {
-        console.log("color black");
         document.querySelector("body").style.backgroundColor = "white";
         document.querySelector("body").style.color = "black";
         document.querySelector(".nav-bar").style.color = "white";
@@ -18,18 +14,23 @@ function change() {
   }
   
   function toggles() {
-    console.log("clicked with funge");
+    let state = document.querySelector(".nav-bar");
+    let positioning = document.querySelector(".menue");
     document.querySelector("#show").onclick = () => {
-      let state = document.querySelector(".nav-bar");
-      let positioning = document.querySelector(".menu");
+        positioning.style.display = "block";
+
       if (state.style.display === "none") {
         state.style.display = "block";
         positioning.style.position = "relative";
         positioning.style.bottom = "70px";
+
   
         state.style.display.scrollBehavior = "smooth";
       } else {
         state.style.display = "none";
+        positioning.style.display = "block";
+        // positioning.style.fontSize = "x-large";
+
       }
     };
   }
